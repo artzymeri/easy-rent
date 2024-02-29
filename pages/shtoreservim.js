b4219b303fc78e42c12e17a3b73858549ea567ce
artzymeri <artzymeri2001@gmail.com>
2024-02-29 14:59:31 +0100

    Added a dialog popup when adding images of car

--- pages/shtoreservim.js
+++ pages/shtoreservim.js
@@ -1,10 +1,16 @@
 import Sidebar from "@/app/Components/Sidebar";
 import React, { useState } from "react";
 import "@/app/Styling/global-styling.css";
-import "@/app/Styling/Reservimet/shtoreservim.css"
+import "@/app/Styling/Reservimet/shtoreservim.css";
 import {
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   FormControl,
+  ImageList,
+  ImageListItem,
   InputLabel,
   MenuItem,
   Select,
@@ -14,6 +20,7 @@ import CarsTest from "@/app/TestingValues/CarsTest";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
+import { AddAPhoto, Close, Delete } from "@mui/icons-material";
 
 const ShtoReservim = () => {
   const [newReservation, setNewReservation] = useState({
@@ -24,6 +31,10 @@ const ShtoReservim = () => {
     startTime: null,
     endTime: null,
   });
+
+  const [images, setImages] = useState([]);
+
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handleCarSelectChange = (carinfo) => {
     setNewReservation({ ...newReservation, carInfo: carinfo.target.value });
@@ -43,12 +54,128 @@ const ShtoReservim = () => {
     });
   };
 
+  const handleFileChange = (event) => {
+    const selectedFiles = event.target.files;
+
+    // Convert each file to base64 and store in an array
+    const promises = [];
+    for (let i = 0; i < selectedFiles.length; i++) {
+      const file = selectedFiles[i];
+      promises.push(
+        new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = (e) => resolve(e.target.result);
+          reader.onerror = (error) => reject(error);
+          reader.readAsDataURL(file);
+        })
+      );
+    }
+
+    // Resolve all promises and update the state with base64 strings
+    Promise.all(promises)
+      .then((base64Images) => {
+        setImages((prevImages) => [...prevImages, ...base64Images]);
+      })
+      .finally(() => {
+        setOpenDialog(true);
+      });
+  };
+
   const addReservation = () => {
     // Here comes the function to add the reservation to backend from newReservation object
+    console.log(images);
   };
 
   return (
     <Sidebar>
+      <Dialog
+        open={openDialog}
+        onClose={() => {
+          setImages([]);
+          setOpenDialog(false);
+        }}
+      >
+        <DialogTitle
+          style={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "center",
+            color: "#015c92",
+          }}
+        >
+          Fotografitë e veturës
+        </DialogTitle>
+        <DialogContent style={{ background: "whitesmoke" }}>
+          <ImageList
+            sx={{ width: 500, height: 450, marginTop: "4px" }}
+            cols={3}
+            rowHeight={164}
+          >
+            {images.map((image, index) => (
+              <ImageListItem
+                style={{
+                  position: "relative",
+                  border: "1px solid #015c92",
+                  borderRadius: "12px",
+                }}
+                key={index}
+              >
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "5px",
+                    left: "5px",
+                    width: "30px",
+                    height: "30px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    background: "white",
+                    borderRadius: "7px",
+                    cursor: "pointer",
+                    border: "1px solid lightgray",
+                  }}
+                >
+                  <Delete sx={{ color: "red" }} />
+                </div>
+                <img
+                  srcSet={image}
+                  src={image}
+                  style={{ objectFit: "contain", objectPosition: "center" }}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </DialogContent>
+        <DialogActions
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => {
+              setImages([]);
+              setOpenDialog(false);
+            }}
+          >
+            Mbyll
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setOpenDialog(false);
+            }}
+          >
+            Ruaj
+          </Button>
+        </DialogActions>
+      </Dialog>
       <h3 style={{ textAlign: "center", fontWeight: "700", color: "#015c92" }}>
         Shto Reservim
       </h3>
@@ -122,12 +249,18 @@ const ShtoReservim = () => {
               onChange={handleEndTime}
             />
           </LocalizationProvider>
-          <input type="file" className="file-uploader" />
+          <input
+            type="file"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+            className="file-uploader"
+          />
           <Button
             fullWidth
             variant="contained"
             onClick={addReservation}
-            sx={{height: '56px'}}
+            sx={{ height: "56px" }}
             size="large"
           >
             Shto Reservimin
